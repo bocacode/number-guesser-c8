2ae95006d9722e5d19998e5a9cd0e9440ac69d83
Damian Montero <photoca@hotmail.com>
2022-10-13 10:28:38 -0400

    guessing is fun

--- guess.js
+++ guess.js
@@ -7,3 +7,28 @@ if it's the right number say "congratulations" and exit
 
 */
 import readline from "readline-sync"
+
+let userGuessedRight = false
+    // guess a number from 1-10
+    let NumberChosen = Math.ceil(Math.random() * 10)
+    //console.log(NumberChosen)
+while(!userGuessedRight) {
+    let guess = readline.question("What is your guess?") //ask for a guess of our number.
+    if (guess == NumberChosen) { // is it equal
+        
+            console.log("congratulations")
+            userGuessedRight = true // get out
+        }
+    if (guess > NumberChosen)
+        {
+            console.log("too high! try again")
+            //userGuessedRight = false
+        }
+    if (guess < NumberChosen)
+        {
+            console.log("too low! try again")
+            //userGuessedRight = false
+        }
+
+}
+
